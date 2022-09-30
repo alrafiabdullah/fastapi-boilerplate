f9ccfb24a4011e452a44840943282ed4075cbc76
alrafiabdullah <abdullah34alrafi@gmail.com>
2022-10-01 05:24:46 +0600

    user model added

---
+++ db/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, String, Integer
+
+from db.config import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    full_name = Column(String)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
