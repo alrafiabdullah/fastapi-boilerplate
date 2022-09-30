eb729f9edbd391ffbb3e69cd2cfdfe5a0dd78acb
alrafiabdullah <abdullah34alrafi@gmail.com>
2022-10-01 05:24:57 +0600

    user model schema added

---
+++ db/schema.py
@@ -0,0 +1,32 @@
+from typing import List, Optional, Generic, TypeVar
+from pydantic import BaseModel, Field
+from pydantic.generics import GenericModel
+
+T = TypeVar('T')
+
+
+class UserSchema(BaseModel):
+    id: Optional[int] = None
+    full_name: Optional[str] = None
+    email: Optional[str] = None
+    password: Optional[str] = None
+
+    class Config:
+        orm_mode = True
+
+
+class Request(GenericModel, Generic[T]):
+    parameter: Optional[T] = Field(None, title="Parameter")
+
+
+class RequestUser(BaseModel):
+    parameter: UserSchema = Field(..., example={
+        "full_name": "Abdullah Al Rafi",
+        "email": "abdullah@al.com"
+    })
+
+
+class Response(GenericModel, Generic[T]):
+    data: Optional[T] = Field(None, title="Data")
+    message: Optional[str] = Field(None, title="Message")
+    code: Optional[int] = Field(None, title="Code")
